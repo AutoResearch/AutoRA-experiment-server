9399fc908a56a2a05631fc100516a19b498ac190
varun646 <varun646@gmail.com>
2024-07-09 12:40:41 -0400

    transition to copier 2.0
    replicate autora-user-cookiecutter functionality with copier
    
    Post gen script:
    - dynamically generates code from jspsych examples if needed
    
    - formats js files using eslint
    
    Note:
    - only dev mode supported
    - Need to remove React elements

--- template/experiment/vite.config.js
+++ template/experiment/vite.config.js
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import eslint from 'vite-plugin-eslint';
 // for the following see https://github.com/cypress-io/cypress/issues/25397#issuecomment-1775454875
 import dns from 'dns';
 dns.setDefaultResultOrder('ipv4first');
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
 	return {
 		build: {
 			outDir: 'dist'
+		},
+		define: {
+			'process.env': env
 		},
 		base: '/experiment/',
 		plugins: [eslint()]
