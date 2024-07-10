5135ee25aa5a1745f1d75e9cfb2b136e40fada11
Katarzyna Rachwal <kasiarachwal@gmail.com>
2024-07-10 08:26:54 -0400

    modifying build instructions

---
+++ build_web_ui.py
@@ -0,0 +1,17 @@
+import os
+from setuptools import Command
+
+class BuildFrontendCommand(Command):
+    description = "Build the frontend using Vite"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.system("cd experiment && npm ci && npm run build")
+        os.system("pwd")
+        os.system("cp build ../server")
