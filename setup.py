2b83087e5a2a60797837b006812ad43abe9614a8
Katarzyna Rachwal <kasiarachwal@gmail.com>
2024-07-11 08:20:28 -0400

    debugging

--- setup.py
+++ setup.py
@@ -1,8 +1,24 @@
 from setuptools import setup
-from build_web_ui import BuildFrontendCommand
+import os
+from setuptools import Command
+
+
+class BuildFrontendCommand(Command):
+    description = "Build the frontend using Vite"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.system("cd experiment && npm ci && npm run build && cp -rf dist ../src")
+
 
 setup(
     cmdclass={
-        'build_ui': BuildFrontendCommand,
+        'build_web_ui': BuildFrontendCommand,
     }
 )
