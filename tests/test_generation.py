f6973f2732f9e2cd08bf72cab076328a3103d7e7
varun646 <varun646@gmail.com>
2024-07-15 15:09:46 -0400

    add support for "None" option, multiselect
    - add tests for selecting multiple theorists/experimentalists
    
    - add documentation

--- tests/test_generation.py
+++ tests/test_generation.py
@@ -1,7 +1,13 @@
 import pytest
 
-theorists = ["autora[theorist-darts]", "autora[theorist-bms]", "autora[theorist-bsr]"]
+theorists = [
+    "None",
+    "autora[theorist-darts]",
+    "autora[theorist-bms]",
+    "autora[theorist-bsr]",
+]
 experimentalists = [
+    "None",
     "autora[experimentalist-inequality]",
     "autora[experimentalist-novelty]",
     "autora[experimentalist-nearest-value]",
@@ -33,16 +39,17 @@ project_type = [
 @pytest.mark.parametrize("theorist", theorists)
 @pytest.mark.parametrize("experimentalist", experimentalists)
 @pytest.mark.parametrize("firebase", firebase)
-@pytest.mark.parametrize("p_type", project_type)
-def test_requirements(copie, theorist, experimentalist, firebase, p_type):
+@pytest.mark.parametrize("proj_type", project_type)
+@pytest.mark.filterwarnings("ignore:Dirty template changes included automatically")
+def test_requirements(copie, theorist, experimentalist, firebase, proj_type):
     """Test that requirements file has necessary dependencies
 
     Args:
-        copie (_type_): _description_
-        theorist (_type_): _description_
-        experimentalist (_type_): _description_
-        firebase (_type_): _description_
-        p_type (_type_): _description_
+        copie (Any): pytest fixture responsible for running copier and cleaning up generated projects
+        theorist (str): additional theorist to install
+        experimentalist (str): additional experimentalists to install
+        firebase (bool): Bool to indicate whether or not to use firebase
+        proj_type (str): type of project to generate
     """
     result = copie.copy(
         extra_answers={
@@ -50,7 +57,7 @@ def test_requirements(copie, theorist, experimentalist, firebase, p_type):
             "theorists": theorist,
             "experimentalists": experimentalist,
             "firebase": firebase,
-            "project_type": p_type,
+            "project_type": proj_type,
         }
     )
 
@@ -68,5 +75,55 @@ def test_requirements(copie, theorist, experimentalist, firebase, p_type):
         reqs = req_file.read()
         reqs_list = reqs.split("\n")
 
-        assert theorist in reqs_list
-        assert experimentalist in reqs_list
+        assert (theorist in reqs_list) == (theorist != "None")
+        assert (experimentalist in reqs_list) == (experimentalist != "None")
+
+
+@pytest.mark.parametrize("firebase", firebase)
+@pytest.mark.parametrize("proj_type", project_type)
+@pytest.mark.filterwarnings("ignore:Dirty template changes included automatically")
+def test_multiselect(copie, firebase, proj_type):
+    """Test project generation and requirements when selecting multiple theorists and experimentalists
+
+    Args:
+        copie (Any): pytest fixture responsible for running copier and cleaning up generated projects
+        firebase (bool): Bool to indicate whether or not to use firebase
+        proj_type (str): type of project to generate
+    """
+    theorists = [
+        "autora[experimentalist-inequality]",
+        "autora[experimentalist-uncertainty]",
+    ]
+    experimentalists = [
+        "autora[experimentalist-inequality]",
+        "autora[experimentalist-novelty]",
+        "autora[experimentalist-nearest-value]",
+    ]
+    result = copie.copy(
+        extra_answers={
+            "project_name": "test",
+            "theorists": str(theorists),
+            "experimentalists": str(experimentalists),
+            "firebase": firebase,
+            "project_type": proj_type,
+        }
+    )
+
+    assert result.exit_code == 0
+    assert result.project_dir.is_dir()
+
+    assert (
+        result.project_dir / "experiment" / "research_hub" / "requirements.txt"
+    ).is_file()
+
+    with open(
+        result.project_dir / "experiment/research_hub/requirements.txt", "r"
+    ) as req_file:
+        reqs = req_file.read()
+        reqs_list = reqs.split("\n")
+
+        for theorist in theorists:
+            assert (theorist in reqs_list) == (theorist != "None")
+
+        for experimentalist in experimentalists:
+            assert (experimentalist in reqs_list) == (experimentalist != "None")
