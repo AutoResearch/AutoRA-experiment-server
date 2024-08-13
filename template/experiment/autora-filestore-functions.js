ca6847153df8b4b663e3668c791890768f49d707
varun646 <varun646@gmail.com>
2024-08-12 22:59:34 -0400

    remove filestore dep for reading conditions

--- template/experiment/autora-filestore-functions.js
+++ template/experiment/autora-filestore-functions.js
@@ -1,5 +1,5 @@
-import autora_metadata from "./data/autora_meta.json" assert { type: 'json' }
-import autora_in_data from "./data/autora_in.json" assert { type: 'json' }
+import autora_metadata from "./conditions_db/autora_meta.json";
+import autora_in_data from "./conditions_db/autora_in.json";
 
 /*
 TODO:
@@ -12,33 +12,52 @@ TODO:
 
 
 const _updateDB = async (study, key, value) => {
-    const fs = require('fs');
-    const autora_metadata_file = './data/autora_meta.json';
-    const file = require(autora_metadata_file);
+    // const fs = require('fs');
+    // const autora_metadata_file = './data/autora_meta.json';
+    // const file = require(autora_metadata_file);
 
-    file.study.key = value;
+    // file.study.key = value;
 
-    fs.writeFile(autora_metadata_file, JSON.stringify(file), function writeJSON(err) {
-        if (err) return console.log(err);
-        console.log(JSON.stringify(file));
-        console.log('writing to ' + autora_metadata_file);
-    });
+    // fs.writeFile(autora_metadata_file, JSON.stringify(file), function writeJSON(err) {
+    // 	if (err) return console.log(err);
+    // 	console.log(JSON.stringify(file));
+    // 	console.log('writing to ' + autora_metadata_file);
+    // });
+    return;
 }
 
-const _updateDoc = async (doc, updated_fields) => {
+const _updateDoc = async (study, key, updated_data) => {
+    autora_metadata[study][key] = updated_data;
+
+    const fs = require(fs);
+
+    const data = JSON.stringify(updated_data);
+
+    // writing the JSON string content to a file
+    fs.writeFile("./conditions_db/autora_meta.json", data, (error) => {
+        // throwing the error
+        // in case of a writing problem
+        if (error) {
+            // logging the error
+            console.error(error);
+
+            throw error;
+        }
+    });
+
     return;
 }
 
 const getCondition = async (study, pId = null) => {
-    for (const key in autora_metadata) {
+    for (let key in autora_metadata[study]) {
         if (autora_metadata[study][key]['start_time'] === null) {
-            const unixTime = Math.floor(Date.now() / 1000);
-            await _updateDB(study, key, { 'start_time': unixTime, 'finished': false, 'pId': pId })
+            let unixTime = Math.floor(Date.now() / 1000);
+            // const updated_value = { 'start_time': unixTime, 'finished': false, 'pId': pId }
+            const condition_str = autora_in_data[study][key];
+            let return_arr = condition_str.slice(1, -1).split(",");
+            return_arr.push(unixTime.toString());
 
-
-            const conditions = autora_in_data[key]
-
-            return key, conditions
+            return return_arr
         }
     }
     return false;
@@ -54,3 +73,5 @@ const setObservation = async (db, study, id, observation, meta = true) => {
     }
     _updateDoc('autora_out', { [id]: observation })
 }
+
+export { getCondition, setObservation }
