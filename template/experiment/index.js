faf7fc72c821a6bdd7af8c66e2536f26a124a3ff
varun646 <varun646@gmail.com>
2024-08-13 16:13:24 -0400

    add post endpoint

--- template/experiment/index.js
+++ template/experiment/index.js
@@ -1,6 +1,10 @@
-import { errorPage } from "./pages";
-import { getCondition } from "./autora-filestore-functions"
+// TODO: remove unused imports once firebase dependency is removed
+
+import { waitPage, endPage, errorPage } from "./pages";
+import { getCondition, setObservation } from "./autora-filestore-functions"
 import main from "./main"
+import axios from "axios";
+
 
 const index = async () => {
     if (process.env.NODE_ENV === 'development' && process.env.NODE_APP_devNoDb === 'True') {
@@ -18,7 +22,18 @@ const index = async () => {
     let condition = await getCondition('autora', prolificId)
     if (condition && (prolificId !== null || process.env.NODE_APP_useProlificId === 'False')) {
         const observation = await main(condition[0], condition[1])
-        return observation;
+        console.log(JSON.stringify(observation))
+
+        axios.post("/data", { key: JSON.parse(observation.json()) })
+            .then((response) => {
+                if (response.status != 200) {
+                    console.warn(response.data.error);
+                }
+                // "links" can also be an array of objects
+
+            })
+            .catch((error) => console.log(error));
+
     } else {
         errorPage()
     }
--- template/server/webserver.py
+++ template/server/webserver.py
@@ -1,22 +1,25 @@
 from pathlib import Path
+from urllib.request import Request
 
 import logger
+import json
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import JSONResponse, FileResponse, RedirectResponse
 
 app = FastAPI()
 
-logger = logger.config(True)
+log = logger.config(True)
 
 app.mount("/experiment", StaticFiles(directory="dist"), name="frontend")
+
 
 @app.get("/")
 def default() -> RedirectResponse:
     """
     Make sure to redirect bare IP/url to form landing page
     """
-    return RedirectResponse(url='/experiment')
+    return RedirectResponse(url="/experiment")
 
 
 @app.get("/experiment")
@@ -25,5 +28,17 @@ def serve_frontend() -> FileResponse:
     Serves static UI
     """
     project_path = Path(__file__).parent.resolve()
-    response = FileResponse(str(project_path / "dist/index.html"), media_type="text/html")
+    response = FileResponse(
+        str(project_path / "dist/index.html"), media_type="text/html"
+    )
     return response
+
+
+@app.post("/data")
+def receive_data(data):
+    # project_path = Path(__file__).parent.resolve()
+
+    # with open(project_path / "autora_out.json", "w") as f:
+    #     json.dump(data, f)
+
+    return data
