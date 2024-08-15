350df0577b78d8129df368c8507e99655f11937c
varun646 <varun646@gmail.com>
2024-08-15 14:54:48 -0400

    fix failing tests

--- template/experiment/index.js
+++ template/experiment/index.js
@@ -3,7 +3,6 @@
 import { waitPage, endPage, errorPage } from "./pages";
 import { getCondition, setObservation } from "./autora-filestore-functions"
 import main from "./main"
-import axios from "axios";
 
 
 const index = async () => {
@@ -21,18 +20,7 @@ const index = async () => {
     let prolificId = "1"
     let condition = await getCondition('autora', prolificId)
     if (condition && (prolificId !== null || process.env.NODE_APP_useProlificId === 'False')) {
-        const observation = await main(condition[0], condition[1])
-        console.log(JSON.stringify(observation))
-
-        axios.post("/data", { key: JSON.parse(observation.json()) })
-            .then((response) => {
-                if (response.status != 200) {
-                    console.warn(response.data.error);
-                }
-                // "links" can also be an array of objects
-
-            })
-            .catch((error) => console.log(error));
+        await main(condition[0], condition[1])
 
     } else {
         errorPage()
